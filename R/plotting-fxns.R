91d4e0c293dd1fbe853528750688681751798e66
wcornwell <w.cornwell@unsw.edu.au>
2013-11-19 11:32:28 +1100

    getting things together

--- R/plotting-fxns.R
+++ R/plotting-fxns.R
@@ -1,6 +1,5 @@
-plot.litFit<-function(x,xlab="Time",ylab="Propotion of mass remaining"){
-  plot(x$mass~x$time,pch=16,xlab=xlab,ylab=ylab,xlim=c(0,max(x$time)))
-  mod<-get(x$model)
+plot.litFit<-function(x,...){
+  plot(x$mass~x$time,pch=16,xlab="Time",ylab="Propotion of mass remaining",xlim=c(0,max(x$time)),...)
+  mod<-eval(parse(text=paste("litterFitter:::",x$model,sep="")))
   lines(seq(0,max(x$time),0.01),do.call(mod, c(list(seq(0,max(x$time),0.01)), as.list(x$optimFit$par))))
-  #add more from James's code here
 }
