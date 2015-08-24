fca7c719dbce7b67021336a8b5aaffc40e8f7ce5
wcornwell <wcornwell@gmail.com>
2015-08-24 13:15:31 +1000

    adding first tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(litterfitter)
+
+test_check("litterfitter")
---
+++ tests/testthat/test-fit_litter.R
@@ -0,0 +1,8 @@
+context("fit_litter")
+
+
+test_that("fit sane", {
+  fit <- fit_litter(time=c(0,1,2,3,4,5,6),mass.remaining=c(1,0.9,1.01,0.4,0.6,0.2,0.01),model="weibull",iters=1000)
+  expect_that(fit, is_a("list"))
+  expect_true(fit$nparams>0)
+})
