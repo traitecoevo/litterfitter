a5d4fbec0c87a6615fa888b4588a0143b1ccd0cf
James Weedon <jtweedon@gmail.com>
2015-09-01 08:58:44 +0200

    last 3 functions and formatting code for readability

--- R/output-fxns.R
+++ R/output-fxns.R
@@ -33,41 +33,41 @@ plot.litfit <- function(x, formulae.cex = 1, ...) {
     
     formula.text <- switch(x$model,
                            
-                           neg.exp = substitute(paste(y==e^A), list(A = paste("-",rnd.to.text(x$optimFit$par, 3),"t",sep=""))),
+                           neg.exp = substitute(paste(y==e^A),
+                                                list(A = paste("-",rnd.to.text(x$optimFit$par, 3),"t",sep=""))),
                            
-                           weibull = substitute(paste(y==e^frac(-t,A)^B), list(A = round(x$optimFit$par[1], 3),B=round(x$optimFit$par[2],3))),
+                           weibull = substitute(paste(y==e^frac(-t,A)^B),
+                                                list(A = round(x$optimFit$par[1], 3),
+                                                     B=round(x$optimFit$par[2],3))),
                            
-                           discrete.parallel = substitute(paste(y==A*e^{B*t}+C*e^{D*t}), list(A=rnd.to.text(x$optimFit$par[1],4),B=rnd.to.text(-1*x$optimFit$par[2],4),C=rnd.to.text(1-x$optimFit$par[1],4),D=rnd.to.text(-1*x$optimFit$par[3],4))),
+                           discrete.parallel = substitute(paste(y==A*e^{B*t}+C*e^{D*t}),
+                                                          list(A=rnd.to.text(x$optimFit$par[1],4),
+                                                               B=rnd.to.text(-1*x$optimFit$par[2],4),
+                                                               C=rnd.to.text(1-x$optimFit$par[1],4),
+                                                               D=rnd.to.text(-1*x$optimFit$par[3],4))),
                            
-                           discrete.series = substitute(paste(y==frac(A*e^{C*t}-D*e^{F*t},G)), list(A=rnd.to.text((1-x$optimFit$par[1])*x$optimFit$par[2]),C=rnd.to.text(-1*x$optimFit$par[3]),D=rnd.to.text(x$optimFit$par[3]-x$optimFit$par[2]*x$optimFit$par[1]),F=rnd.to.text(-1*x$optimFit$par[2]),G=x$optimFit$par[2] - x$optimFit$par[3])),
+                           discrete.series = substitute(paste(y==frac(A*e^{C*t}-D*e^{F*t},G)),
+                                                        list(A=rnd.to.text((1-x$optimFit$par[1])*x$optimFit$par[2]),
+                                                             C=rnd.to.text(-1*x$optimFit$par[3]),
+                                                             D=rnd.to.text(x$optimFit$par[3]-x$optimFit$par[2]*x$optimFit$par[1]),
+                                                             F=rnd.to.text(-1*x$optimFit$par[2]),
+                                                             G=x$optimFit$par[2] - x$optimFit$par[3])),
                            
-                           cont.quality.1 = NULL,
+                           cont.quality.1 = substitute(paste(y==frac((B^A), (B+t)^A)),
+                                                       list(A=rnd.to.text(x$optimFit$par[2]),
+                                                            B=rnd.to.text(x$optimFit$par[1]))),
                            
-                           cont.quality.2 = NULL,
+                           cont.quality.2 = substitute(paste(y==frac(1, (1+B*t)^A)),
+                                                       list(A=rnd.to.text(x$optimFit$par[2]),
+                                                            B=rnd.to.text(x$optimFit$par[1]))),
                            
-                           neg.exp.limit = NULL)
+                           neg.exp.limit = substitute(paste(y==A*e^{-K*t}+B),
+                                                      list(K=rnd.to.text(x$optimFit$par[1]),
+                                                           A=rnd.to.text(x$optimFit$par[2]),
+                                                           B=rnd.to.text(x$optimFit$par[3])))
+                           )
     
     text(pt.pos[1], pt.pos[2], label = formula.text, cex=formulae.cex)
-## THE FUNCTIONS BELOW NEED TO BE CONVERTED TO THE STYLE AS ABOVE
-    
-#     tmp = switch(x$model, 
-#         discrete.parallel = sprintf("text(%f,%f, expression(paste(y==%s(\"%.4fe\"^-'%.4f t'%s\"%.4fe\"^-'%.4f t')/%.4f)),cex=%f)", 
-#             pt.p = pt.pos[1], pt.pos[2], ifelse(x$optimFit$par[2] - x$optimFit$par[3] > 
-#                 0, "", "-"), (1 - x$optimFit$par[1]) * x$optimFit$par[2], x$optimFit$par[3], 
-#             ifelse(x$optimFit$par[3] - x$optimFit$par[2] < 0, "+", "-"), abs(x$optimFit$par[3] - 
-#                 x$optimFit$par[2] * x$optimFit$par[1]), x$optimFit$par[2], abs(x$optimFit$par[2] - 
-#                 x$optimFit$par[3]), formulae.cex), discrete.series = sprintf("text(%f,%f, expression(paste(y==\"%.4fe\"^-'%.4f t'+\"%.4fe\"^-'%.4f t')),cex=%f)", 
-#             pt.pos[1], pt.pos[2], x$optimFit$par[1], x$optimFit$par[2], 1 - x$optimFit$par[1], 
-#             x$optimFit$par[3], formulae.cex), cont.quality.1 = sprintf("text(%f,%f, expression(paste(y==%.4f^%.4f*(%.4f+t)^%.4f)),cex=%f)", 
-#             pt.pos[1], pt.pos[2], fit$optimFit$par[1], fit$optimFit$par[2], fit$optimFit$par[1], 
-#             fit$optimFit$par[2] * (-1), formulae.cex), cont.quality.2 = sprintf("text(%f,%f, expression(paste(y==\"(1%s%.4f t)\"^%.4f)),cex=%f)", 
-#             pt.pos[1], pt.pos[2], ifelse(fit$optimFit$par[1] > 0, "+", "-"), abs(fit$optimFit$par[1]), 
-#             fit$optimFit$par[2] * (-1), formulae.cex), neg.exp.limit = sprintf("text(%f,%f, expression(paste(y==\"%.4fe\"^-'%.4f t' %s %.4f)),cex=%f)", 
-#             pt.pos[1], pt.pos[2], fit$optimFit$par[2], fit$optimFit$par[1], ifelse(fit$optimFit$par[3] > 
-#                 0, "+", "-"), abs(fit$optimFit$par[3]), formulae.cex))
-#     
-#     writeLines(tmp, "abcdefg.r")
-#     source("abcdefg.r")
 } 
 
 #' @export
