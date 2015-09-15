3f328918cb741333be899437354e36371001d332
James Weedon <jtweedon@gmail.com>
2015-09-15 14:14:30 +0200

    adding some tests

--- tests/testthat/test-fit_litter.R
+++ tests/testthat/test-fit_litter.R
@@ -1,5 +1,5 @@
 context("fit_litter")
-
+data("pineneedles")
 
 test_that("fit sane", {
     fit <- fit_litter(time = c(0, 1, 2, 3, 4, 5, 6), mass.remaining = c(1, 0.9, 1.01, 0.4, 0.6, 0.2, 0.01), model = "weibull", iters = 1000)
@@ -35,5 +35,14 @@ test_that("crazy input throws errors", {
     expect_error(fit_litter(time = c(0, 1, 2, 3, 4 ), mass.remaining = c(1, 0.9, 1.01, 0.4, 0.6), model = "weibull", iters = 10,lower=2))
 })
 
-
+test_that("known parameter fits on boundary", {
+ expect_warning(fit_litter(time=pineneedles$Year,mass.remaining = pineneedles$Mass.remaining,
+                           model = "cont.quality.1",iters = 200))
+  expect_warning(fit_litter(time=pineneedles$Year,mass.remaining = pineneedles$Mass.remaining,
+                            model = "discrete.series",iters = 200))
+  expect_warning(fit_litter(time=pineneedles$Year,mass.remaining = pineneedles$Mass.remaining,
+                            model = "discrete.parallel",iters = 200))
+  
+  
+})
  
